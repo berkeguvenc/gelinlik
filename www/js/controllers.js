87becf043d0bc2d1b626f88129b6074b66eddfcb
JitanX <anilsolmaz@gmail.com>
2020-03-01 14:59:01 +0300

    anilin node_modules dosyaları

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,12 +2,17 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+$rootScope.webServiceUrl = "https://anilsolmaz.com/gelinlik/webservice.php"
+
+        var ServiceRequest = {
+            service_type: "detaylar"
+        }
+
+        // Yeni user isteği post edilir ve veritabanına eklenir.
+        $http.post($rootScope.webServiceUrl, ServiceRequest).success(function(data) {
+            $scope.detaylar = data
+        })
+      
 
   
 })
@@ -19,11 +24,5 @@ angular.module('starter.controllers', [])
 
   $scope.deneme = $stateParams.detayId
 
-  $scope.gelenveri = [
-  {"aciklama":"deneme", "resimURL":"https://i.dugun.com/gallery/g_47711/prev-w2_nisantasi-gelinlik-firmalari-ve-fiyatlari_nrAXQ64d.jpg"},
-  {"aciklama":"deneme", "resimURL":"https://i.dugun.com/gallery/g_47711/prev-w2_nisantasi-gelinlik-firmalari-ve-fiyatlari_nrAXQ64d.jpg"},
-  {"aciklama":"deneme", "resimURL":"https://cdn.olegcassini.com.tr/cicek-islemeli-dusuk-omuzlu-prenses-gelinlik-indirim-dreamon-for-oleg-cassini-8873-43-B.jpg"}, 
-  {"aciklama":"deneme", "resimURL":"https://cdn.olegcassini.com.tr/cicek-islemeli-dusuk-omuzlu-prenses-gelinlik-indirim-dreamon-for-oleg-cassini-8873-43-B.jpg"}
-  
-  ];
+ var Service
 });
